08c9c4148b083a48b66a30488d58e266bb976c1d
Edward Tanguay (windows) <edward@tanguay.info>
2022-07-13 14:24:01 +0200

    with logout

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import { JobSource } from './models/JobSource.js';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import jwt from 'jsonwebtoken';
 
 const user = {
     id: 1,
@@ -25,15 +26,52 @@ app.use(cors());
 app.use(express.json());
 const port = process.env.PORT || 3045;
 
+const verifyToken = (req, res, next) => {
+    const bearerHeader = req.headers['authorization'];
+    if (typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(' ');
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    } else {
+        res.sendStatus(403);
+    }
+};
+
+const decodeJwt = (token) => {
+    let base64Url = token.split('.')[1];
+    let base64 = base64Url.replace('-', '+').replace('_', '/');
+    let decodedData = JSON.parse(Buffer.from(base64, 'base64').toString('binary'));
+    return decodedData;
+}
+
 app.get('/', (req, res) => {
     res.send('<h1>EJ2 Job Manager API</h1>');
+});
+
+app.post('/maintain-login', verifyToken, (req, res) => {
+    jwt.verify(req.token, 'secretkey', (err, authData) => {
+        if (err) {
+            res.sendStatus(403);
+        } else {
+            const data = decodeJwt(req.token);
+            res.json({
+                user: data.user
+            });
+        }
+    });
 });
 
 app.post('/login', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
     if (username === 'hans' && password === '123') {
-        res.json({ user });
+        jwt.sign({ user }, 'secretkey', { expiresIn: '20s' }, (err, token) => {
+            res.json({
+                user,
+                token
+            });
+        })
     } else {
         res.sendStatus(403);
     }
